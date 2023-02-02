59fb2e9108eb6b363f01a6a09b572965b6697bc4
Isaac Ayeni <ayeniisaac1on1@gmail.com>
2023-02-02 07:11:03 +0100

    02-02-2023

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+
+const hiddenElements =document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('app-show')
+        }else{
+            entry.target.classList.remove('app-show')
+        }
+    })
+})
+
+
+const hiddenElements1 =document.querySelectorAll('.app-hidden');
+hiddenElements.forEach((el) => observer.observe(el));
